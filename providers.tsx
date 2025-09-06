5d2640374ea9c28842932a917f99288d628d7789
Yigit Yektin <yigit1yektin@gmail.com>
2025-09-06 13:30:47 +0300

    [ADD] nextjs, onchainkit

---
+++ providers.tsx
@@ -0,0 +1,17 @@
+// @noErrors: 2307 2580 2339 - cannot find 'process', cannot find './wagmi', cannot find 'import.meta'
+'use client';
+
+import type { ReactNode } from 'react';
+import { OnchainKitProvider } from '@coinbase/onchainkit';
+import { base } from 'wagmi/chains'; // add baseSepolia for testing
+
+export function Providers(props: { children: ReactNode }) {
+  return (
+    <OnchainKitProvider
+      apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+      chain={base} // add baseSepolia for testing
+    >
+      {props.children}
+    </OnchainKitProvider>
+  );
+}
